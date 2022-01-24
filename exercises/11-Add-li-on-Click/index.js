058ce39ea67b5ff17c5e944cbac5494a6749094d
itrytocodethings <imwaynebailey@gmail.com>
2022-01-24 22:41:03 +0000

    completed

--- exercises/11-Add-li-on-Click/index.js
+++ exercises/11-Add-li-on-Click/index.js
@@ -1,5 +1,17 @@
 let button = document.querySelector("#superDuperButton");
+let ul = document.querySelector('#myList');
+let count = 0;
 button.addEventListener("click", function() {
-	//your code here
-
+	let suffixList = ['st', 'nd', 'rd', 'th'];
+	let suffix;
+	let countArr = count.toString().split('');
+	let last = count.toString().split('').length - 1;
+	if (count == 11 || count == 12 || count == 13) suffix = suffixList[3];
+	else if (countArr[last] == 0 && countArr.length == 1) suffix = '';
+	else if(countArr[last] == 1) suffix = suffixList[0];
+	else if(countArr[last] == 2) suffix = suffixList[1];
+	else if(countArr[last] == 3) suffix = suffixList[2];
+	else suffix = suffixList[3];
+	ul.innerHTML += `<li>${count}${suffix} element</li>`
+	count++;
 });
--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,19 @@
-// Your code here
+//add task
+let ul = document.querySelector('ul');
+let toDoInput = document.querySelector('#addToDo')
+toDoInput.addEventListener('keypress', (e) => {
+    if (e.keyCode == 13 && toDoInput.value && (/[a-zA-Z]/).test(toDoInput.value)) { 
+        ul.innerHTML += `<li><span><i class="fa fa-trash"></i></span>${toDoInput.value}</li>`
+        toDoInput.value = '';
+    }
+})
+
+//remove task
+ul.addEventListener('click', (e) => {
+    console.log(e)
+    if (e.target.nodeName == 'I' || e.target.nodeName == 'SPAN') {
+        let li = e.composedPath().filter((element) => element.nodeName == 'LI');
+        li[0].remove();
+    } 
+    else if (e.target.nodeName == 'LI') e.target.remove();
+})
